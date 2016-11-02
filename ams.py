8442fa241556e00b672575f0b4ac05fdec1394b3
abk <abk@wlan-145-94-140-112.wlan.tudelft.nl>
2016-11-02 16:11:46 +0100

    Fixed First Edition
    Geo condition works.
    Tweets are separated when writing them into a json.

--- ams.py
+++ ams.py
@@ -4,6 +4,7 @@ from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
 
+import json
 
 consumer_key = "Xv9W0XpiZxntW9ntXcBjf1bvo"
 consumer_secret = "JH5KQ3eYiEsrVOccpPIyurJe77A7a4xOgiDMOx4DvbJ8jDTDRY"
@@ -11,22 +12,21 @@ consumer_secret = "JH5KQ3eYiEsrVOccpPIyurJe77A7a4xOgiDMOx4DvbJ8jDTDRY"
 access_token = "793735961691746304-nVMRnoVguoz6v62jdYYMdKAmELQU4AC"
 access_token_secret = "Miu9EXCrc34yGZr6nkWm3JA8rvSlyTHPFOZMpEyOlOeuB"
 
-geobox_amsterdam = [4. 73,52.29,4.98,52.42]
+geobox_amsterdam = [4.73, 52.29, 4.98, 52.42]
+file = open('ams.json', 'a')
+
 
 class StdOutListener(StreamListener):
 
-	def on_status(self,data):
-		print("\\")
+	def on_data(self,data):
 		print(data)
-		print("\\")
-		return True
+		json_data = json.loads(data)
+		file.write(str(json_data))
+		file.write('\r\n')
 
 	def on_error(self,status):
-		print("\\")
 		print(status)
-		print("\\")
 
-startTimeStamp = time.clock()
 
 if __name__ == '__main__':
 
@@ -37,4 +37,3 @@ if __name__ == '__main__':
 	stream = Stream(auth,l)
 	stream.filter(locations=geobox_amsterdam)
 
-print('End Streaming')
